51ce4530cc436ff76010ad4fe9148709a513b3ab
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-03-26 18:57:19 +0200

    adding components

---
+++ src/Components/Weather/Weather.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import WeatherSearch from "./WeatherSearch";
+
+class Weather extends React.Component {
+    render(){
+        return(
+            <div>
+                <WeatherSearch/>
+            </div>
+        )
+    }
+}
+
+export default Weather
---
+++ src/Components/Weather/WeatherSearch.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class WeatherSearch extends React.Component {
+    render() {
+        return(
+            <div>
+                <form>
+                    <input
+                        type='text'
+                    >
+
+                    </input>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default WeatherSearch
